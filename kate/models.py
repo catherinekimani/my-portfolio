7559166b423803d18b07d2aa0d868c8faa1beb6a
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-26 22:09:27 +0300

    Ft: Create profile model

--- kate/models.py
+++ kate/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
+class Profile(models.Model):
+    profile_pic = CloudinaryField("pic")
+    name = models.CharField(max_length=100)
+    about = models.TextField()
+    contact = models.EmailField(max_length=100)
+    resume = CloudinaryField("resume")
+    github = models.URLField(max_length=200)
+    linkedin = models.URLField(max_length=200)
+    facebook = models.URLField(max_length=200)
+    twitter = models.URLField(max_length=200)
+    
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()
+    
+    def __str__(self):
+        return f'{self.name}'
