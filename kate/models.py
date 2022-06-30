05cdd129ed7efa913d70e795268ac76f9f198177
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-30 08:32:02 +0300

    Ft: Create Contact model class

--- kate/models.py
+++ kate/models.py
@@ -65,3 +65,23 @@ class Project(models.Model):
     
     def __str__(self):
         return f'{self.project_img}'
+    
+class Contact(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    contact_img = CloudinaryField('img')
+    created_at = models.DateTimeField(auto_now_add=True)
+    contact = models.EmailField(max_length=100)
+    github = models.URLField(max_length=200)
+    linkedin = models.URLField(max_length=200)
+    facebook = models.URLField(max_length=200)
+    twitter = models.URLField(max_length=200)
+    
+    def save_project(self):
+        self.save()
+        
+    def delete_project(self):
+        self.delete()
+    
+    def __str__(self):
+        return f'{self.title}'
