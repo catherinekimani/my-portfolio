8d29331e0ecb6fa21551ce46cb08e8a1db357449
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-06-30 08:10:03 +0300

    Ft: Create Project model class

--- kate/models.py
+++ kate/models.py
@@ -50,3 +50,18 @@ class Skills(models.Model):
     
     def __str__(self):
         return f'{self.skills}'
+    
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    project_img = CloudinaryField('img')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def save_project(self):
+        self.save()
+        
+    def delete_project(self):
+        self.delete()
+    
+    def __str__(self):
+        return f'{self.project_img}'
